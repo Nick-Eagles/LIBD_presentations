1c6f436a6a6347f8046d2d899cf2e7c42bc6ee2b
Nick-Eagles <nick.eagles@libd.org>
2022-06-22 16:36:42 -0400

    Add some commentary explaining the purpose and context of the script

--- rstats_anndata/01-prepare_data.sh
+++ rstats_anndata/01-prepare_data.sh
@@ -1,5 +1,13 @@
+#   This script is intended for use at JHPCE, where local files are linked into
+#   this repository in preparation for later analysis. Note that all data used
+#   here is public, so the appropriate files may also be obtained following the
+#   guidelines here:
+#
+#   http://research.libd.org/spatialLIBD/#raw-data
+
 src_dir=/dcl02/lieber/ajaffe/SpatialTranscriptomics/HumanPilot/10X
 sample=151507
 
+#   Symlink outputs from spaceranger that we'll reference later
 ln -s $src_dir/$sample/tissue_hires_image.png data/
 ln -s $src_dir/$sample/scalefactors_json.json data/
