91518e8765cbe92fd56d0793f826b3c6aaf1907f
Nick-Eagles <nick.eagles@libd.org>
2022-06-22 16:27:28 -0400

    Add a script to prepare input data for use at the JHPCE cluster

---
+++ 01-prepare_data.sh
@@ -0,0 +1,5 @@
+src_dir=/dcl02/lieber/ajaffe/SpatialTranscriptomics/HumanPilot/10X
+sample=151507
+
+ln -s $src_dir/$sample/tissue_hires_image.png data/
+ln -s $src_dir/$sample/scalefactors_json.json data/
