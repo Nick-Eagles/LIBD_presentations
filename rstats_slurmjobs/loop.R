5f0fbfe24516e8cbc20c65f04720284ff9148037
Nick-Eagles <nick.eagles@libd.org>
2023-10-25 15:17:39 -0400

    Create scripts in the correct directory using 'with_dir'; use fewer cell types and manually enumerate them

--- rstats_slurmjobs/loop.R
+++ rstats_slurmjobs/loop.R
@@ -1,18 +1,19 @@
 library(spatialLIBD)
 library(here)
 library(slurmjobs)
+library(stringr)
+library(withr)
+
+script_name = "deconvo_plots"
 
 spe = fetch_data(type = "spatialDLPFC_Visium")
 
-#   The names of the colData columns related to broad cell-type counts
-full_cell_types = colnames(colData(spe))[
-    grep('^broad_cell2location_', colnames(colData(spe)))
-]
-print(full_cell_types)
-
-#   Shortened cell-type names that are more helpful for logging
-short_cell_types = str_extract(full_cell_types, '_([a-z]+)$', group = 1)
-print(short_cell_types)
+#   Neuronal cell-type-count predictions from cell2location, shortened from the
+#   full column names in colData(spe)
+cell_types = c("inhib", "excit")
+stopifnot(
+    all(paste0('broad_cell2location_', cell_types) %in% colnames(colData(spe)))
+)
 
 #   Suppose we want to plot just the anterior position in the DLPFC
 sample_ids = unique(spe$sample_id)[grep('_ant$', unique(spe$sample_id))]
@@ -20,18 +21,20 @@ print(sample_ids)
 
 #   Let's run job_loop with create_shell = FALSE, to take a quick peek at the
 #   shell and R scripts that would be created
-script_pair = job_loop(
-    loops = list(
-        cell_type = short_cell_types,
-        sample_id = sample_ids
-    ),
-    name = "deconvo_plots",
-    partition = "shared",
-    memory = "5G",
-    cores = 1,
-    tc = 10,
-    logdir = "logs",
-    create_shell = FALSE
+with_dir(
+    here("rstats_slurmjobs"),
+    {
+        script_pair = job_loop(
+            loops = list(cell_type = cell_types, sample_id = sample_ids),
+            name = script_name,
+            partition = "shared",
+            memory = "5G",
+            cores = 1,
+            tc = 10,
+            logdir = "logs",
+            create_shell = FALSE
+        )
+    }
 )
 
 cat(script_pair[["shell"]], sep = "\n")
@@ -39,24 +42,26 @@ cat(script_pair[["R"]], sep = "\n")
 
 #   Though in practice, you'll likely just write the scripts directly with the
 #   'job_loop' function, using create_shell = TRUE
-script_pair = job_loop(
-    loops = list(
-        cell_type = short_cell_types,
-        sample_id = sample_ids
-    ),
-    name = "deconvo_plots",
-    partition = "shared",
-    memory = "5G",
-    cores = 1,
-    tc = 10,
-    logdir = "logs",
-    create_shell = TRUE
+with_dir(
+    here("rstats_slurmjobs"),
+    {
+        script_pair = job_loop(
+            loops = list(cell_type = cell_types, sample_id = sample_ids),
+            name = script_name,
+            partition = "shared",
+            memory = "5G",
+            cores = 1,
+            tc = 10,
+            logdir = "logs",
+            create_shell = TRUE
+        )
+    }
 )
 
 #   To resubmit failed tasks from the above array, run with submit = TRUE. Here,
 #   we'll just examine which tasks failed and how that was determined
 array_submit(
-    'deconvo_plots.sh',
+    name = script_name,
     submit = FALSE,
     verbose = TRUE
 )
