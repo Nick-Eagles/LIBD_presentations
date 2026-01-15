21c200e93560924ca3565279d01443873c5de395
Nick-Eagles <nick.eagles@libd.org>
2026-01-15 16:44:02 -0500

    Output job IDs to a text file

--- rstats_dplyr_benchmark/submit_runs.sh
+++ rstats_dplyr_benchmark/submit_runs.sh
@@ -19,16 +19,23 @@ echo "Job name: ${SLURM_JOB_NAME}"
 echo "Node name: ${HOSTNAME}"
 echo "Task id: ${SLURM_ARRAY_TASK_ID}"
 
+mkdir -p results
+
 script_names=(dplyr_fread.sh duckplyr_fread.sh pure_dplyr.sh pure_duckplyr.sh)
 num_cores=(1 2 4 8)
 
 for script_name in "${script_names[@]}"; do
     for cores in "${num_cores[@]}"; do
-        sbatch \
-            -c "$cores" \
-            -o logs/"${script_name%.sh}_c${cores}.txt" \
-            -e logs/"${script_name%.sh}_c${cores}.txt" \
-            "$script_name"
+        job_id=$(
+            sbatch
+                --parsable
+                -c $cores
+                -o logs/${script_name%.sh}_c${cores}.txt
+                -e logs/${script_name%.sh}_c${cores}.txt
+                $script_name
+        )
+        echo "$job_id" >> results/job_ids.txt
+        echo "Submitted ${script_name} with ${cores} cores: job ID $job_id"
     done
 
     sleep 1000
