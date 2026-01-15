0ac0c4276221c7c278435ec67a841c4652092319
Nick-Eagles <nick.eagles@libd.org>
2026-01-15 16:34:13 -0500

    Add a script to submit each of the task options

---
+++ rstats_dplyr_benchmark/submit_runs.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#SBATCH -p katun
+#SBATCH --mem=2G
+#SBATCH --job-name=submit_runs
+#SBATCH -t 1-0:00:00
+#SBATCH -o logs/submit_runs.txt
+#SBATCH -e logs/submit_runs.txt
+#SBATCH -c 1
+
+set -e
+
+echo "**** Job starts ****"
+date
+
+echo "**** JHPCE info ****"
+echo "User: ${USER}"
+echo "Job id: ${SLURM_JOB_ID}"
+echo "Job name: ${SLURM_JOB_NAME}"
+echo "Node name: ${HOSTNAME}"
+echo "Task id: ${SLURM_ARRAY_TASK_ID}"
+
+script_names=(dplyr_fread.sh duckplyr_fread.sh pure_dplyr.sh pure_duckplyr.sh)
+num_cores=(1 2 4 8)
+
+for script_name in "${script_names[@]}"; do
+    for cores in "${num_cores[@]}"; do
+        sbatch \
+            -c "$cores" \
+            -o logs/"${script_name%.sh}_c${cores}.txt" \
+            -e logs/"${script_name%.sh}_c${cores}.txt" \
+            "$script_name"
+    done
+
+    sleep 1000
+done
+
+echo "**** Job ends ****"
+date
